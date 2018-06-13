4d61fcda215e441160216a3597820f18354a87d2
ArthurHwang <ahwang55@gmail.com>
2018-06-13 11:28:57 -0700

    Create a variable that holds an img element and add it to the page #1

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 class Car {
   constructor($img, speed, direction, location) {
-    this.$img = $img;
-    this.speed = speed;
-    this.direction = direction;
-    this.location = location;
+    this.$img = $img
+    this.speed = speed
+    this.direction = direction
+    this.location = location
   }
 }
+
+
+const image = document.createElement('img')
+image.src = 'https://opengameart.org/sites/default/files/simple-travel-car-top_view.svg'
+
+document.body.appendChild(image)
